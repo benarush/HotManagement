73758b8d5981a9141af1da424ba48a9dafc9be3f
MSI\benha <benharushtomer@gmail.com>
2020-12-17 01:16:15 +0200

    starting working on Task Details , add front to detect redirect after create/edit task_details on task_details.html

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('alltasks/create_task/' , TaskCreateView.as_view(), name="create-task"),
     path('alltasks/edit_task/<int:pk>', TaskUpdateView.as_view(), name='edit-task'),
     path('alltasks/details_task/<int:pk>', TaskDetailsView.as_view(), name="task-details"),
+    path('alltasks/create_details_task/<int:pk>', TaskDetailsCreateView.as_view(), name = 'create-task-details'),
     path('alltasks/delete_task/<int:pk>', TaskDeleteView.as_view(), name="delete-task"),
     path('alltasks/', AllTasksViews.as_view(), name='all-tasks')
 ]
--- Tasks/views.py
+++ Tasks/views.py
@@ -87,6 +87,8 @@ class TaskDetailsView(UserPassesTestMixin,LoginRequiredMixin,DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(TaskDetailsView, self).get_context_data(**kwargs)  # get the default context data
+        task = self.get_object()
+        context['task_details'] = TaskDetail.objects.filter(task=task)
         return context
 
     def test_func(self):
@@ -96,9 +98,78 @@ class TaskDetailsView(UserPassesTestMixin,LoginRequiredMixin,DetailView):
             return True
         return False
 
+
+
+class TaskDetailsCreateView(LoginRequiredMixin, CreateView):
+    model = TaskDetail
+#   The Field Variable Tell CreateView Class witch field that we want to update on the creation
+    template_name = "Tasks/task_details/create.html"
+    fields = ['problem', 'mission', 'responsibility', 'email', 'status']
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskDetailsCreateView, self).get_context_data(**kwargs)  # get the default context data
+        return context
+
+    def form_valid(self, form):
+#       this will set the form instance author to the user that log in , in the request
+        form.instance.task = self.request.GET
+        return super().form_valid(form)
+
+
+# class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+#     model = Task
+#     fields = ['problem', 'weeks', 'description', 'email_attached_file', 'start_date']
+#     def get_context_data(self, **kwargs):
+#         context = super(TaskUpdateView, self).get_context_data(**kwargs)  # get the default context data
+#         return context
+#
+#     def form_valid(self, form):
+#         #       this will set the form instance author to the user that log in , in the request
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
+#
+#     def test_func(self):
+# #   The get_object() Will Get the object that we want to update.
+#         task = self.get_object()
+#         if self.request.user == task.author:
+#             return True
+#         return False
+
+
+# class TaskDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+#     model = Task
+#     context_object_name = "task"
+#     success_url = '/'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(TaskDeleteView, self).get_context_data(**kwargs)  # get the default context data
+#         return context
+#
+# #   this method will check if the user author and the user that log in are same.
+#     def test_func(self):
+# #   The get_object() Will Get the object that we want to update.
+#         post = self.get_object()
+#         if self.request.user == post.author:
+#             return True
+#         return False
+
+
+# class TaskDetailsDetailsView(UserPassesTestMixin,LoginRequiredMixin,DetailView):
+#     model = TaskDetail
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(TaskDetailsView, self).get_context_data(**kwargs)  # get the default context data
+#         return context
+#
+#     def test_func(self):
+# #   The get_object() Will Get the object that we want to update.
+#         task_details = self.get_object()
+#         if self.request.user == task_details.author:
+#             return True
+#         return False
+
+
 def home(request):
     context = {'title': 'About Me!',
                }
     return render(request, 'Tasks/home.html', context)
-
-
